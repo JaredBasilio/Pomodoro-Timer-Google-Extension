fcffd9e6b15057c0146aa776274fb391e64fc2c1
Jared Basilio <jaredbasilio02@gmail.com>
2022-01-05 23:52:56 -0800

    background timer

--- background.js
+++ background.js
@@ -1,18 +1,118 @@
 let timerID;
-let timerTime;
-let sound;
+let timerTime; //future time
+let timeLeft; //amount left
+
+let numShort = 0;
+let partition = 0;
+
+let shortBreak;
+let longBreak;
+
+/**
+ * 
+    clearInterval(interval);
+    interval = null;
+    startTime();
+ */
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.cmd === 'START_TIMER') {
     timerTime = new Date(request.when); //date object for incoming time
+    shortBreak = request.short;
+    longBreak = request.long;
+
     console.log('started timer to end at: ' + timerTime);
     timerID = setTimeout(() => {
-       console.log('time up')
+      //plays the sound
+      playSound();
+
+      //sends a message to popup that the timeout has ended
+      if (numShort < 4) {
+        partition = (partition + 1) % 2;
+      } else { //starts the long timer
+        numShort = 0;
+        partition = 2;
+      }
+
     }, timerTime.getTime() - Date.now());
   } else if (request.cmd === 'GET_TIME') {
-    console.log("got time: " + timerTime);
+    if (timerTime) {
+      console.log('Time Resumed: ' + msToTime(timerTime - Date.now()));
+    }
     sendResponse({ 
-      time: timerTime 
+      time: timerID ? timerTime : timeLeft + Date.now(),
+      running: timerID
     });
+  } else if (request.cmd === 'END_TIMER') {
+    clearTimeout(timerID);
+    timeLeft = null;
+    timerTime = null;
+    console.log("session ended")
+  } else if (request.cmd === 'RUNNING') {
+    if (timerID) { //the timer is currently running
+      timeLeft = timerTime - Date.now()
+      timerID = null;
+
+      console.log(`session is running, paused with ${msToTime(timeLeft)} time left`)
+
+      sendResponse({
+        cmd: 'PAUSE',
+        time: timeLeft
+      });
+    } else if (timeLeft) { //the timer is paused
+      timerID = setTimeout(() => { //restarts the timeout with the new ending time
+        playSound();
+
+        if (numShort < 4) {
+          partition = (partition + 1) % 2;
+        } else { //starts the long timer
+          numShort = 0;
+          partition = 2;
+        }
+
+      }, timeLeft + Date.now());
+
+      sendResponse({
+        cmd: 'RESUME',
+        time: timeLeft + Date.now()
+      });
+      
+      console.log(`session resumed with ${msToTime(timeLeft)}, new end time: ${msToTime(timeLeft + Date.now())}`);
+    } else { //there is no current session
+      console.log("session not running")
+    }
   }
 });
+
+function playSound() {
+  let url = chrome.runtime.getURL('audio.html');
+
+  // set this string dynamically in your code, this is just an example
+  // this will play success.wav at half the volume and close the popup after a second
+  url += '?volume=0.5&src=sounds/digital.mp3&length=1000';
+
+  chrome.windows.create({
+      type: 'popup',
+      focused: false,
+      top: 1,
+      left: 1,
+      height: 1,
+      width: 1,
+      url,
+  })
+
+}
+
+//converts ms time to readable hours:minutes:seconds:ms
+function msToTime(duration) {
+  var milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 let interval;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('listened')
-  const time = Date(response.time);
-  startTimer(time);
-});
-
 //when the pop-up is opened this is called
 chrome.runtime.sendMessage({ cmd: 'GET_TIME' }, response => {
-    const time = Date(response.time);
-    startTimer(time);
+  if (response.time) {
+    const time = new Date(response.time);
+    console.log(response.running);
+    if (response.running) {
+      console.log('timer resumed at: '+ time);
+      startTimer(time);
+    } else {
+      timeConversion(time.getTime() - Date.now());
+    }
+  }
 });
 
 let partition = 0;
@@ -19,44 +21,78 @@ let numShort = 0;
 // increments by 1 every short break, resets to 0 after 4
 
 const countdownEl = document.getElementById('countdown');
+countdownEl.innerHTML = `00:00`;
 
 document.getElementById("start").addEventListener("click", startTime);
+document.getElementById("pause").addEventListener("click", pauseTime);
+document.getElementById("end").addEventListener("click", endSess);
 
 function startTimer(time) {
   if (time.getTime() > Date.now()) {
     interval = setInterval(() => {
+      //calculates the time remaining from the current time and the proposed
+      //end time
       let timeMS = time.getTime() - Date.now();
-      if (time >= 0) {
-        const minutes = Math.floor(timeMS / 60000);
-        let seconds = ((timeMS % 60000) / 1000).toFixed(0);
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-    
-        countdownEl.innerHTML = `${minutes}:${seconds}`;
+      if (timeMS >= 0) {
+        timeConversion(timeMS);
         timeMS -= 1000;
       } else {
-        clearInterval(interval);
-        interval = null;
-        if (numShort < 4) {
-          partition = (partition + 1) % 2;
-        } else {
-          numShort = 0;
-          partition = 2;
-        }
-        startTime();
+        //transition()
+        
       }
     }, 1000);
   }
 }
 
+function transition() {
+  clearInterval(interval);
+  interval = null;
+  if (numShort < 4) {
+    partition = (partition + 1) % 2;
+  } else { //starts the long timer
+    numShort = 0;
+    partition = 2;
+  }
+  startTime();
+}
+
 function startTime() { //function for the button
-  startingMinutes = document.getElementById(currEvent()).value;
-  sound = document.getElementById("sound");
-  
-  let time = new Date(Date.now() + startingMinutes * 60000); //future date object
+  workMinutes = document.getElementById("workTime").value;
+  shortMinutes = document.getElementById("sBreak").value;
+  longMinutes = document.getElementById("lBreak").value;
   
   //sent to background.js the future time in ms
-  chrome.runtime.sendMessage({cmd: 'START_TIMER', when: time})
+  chrome.runtime.sendMessage({cmd: 'START_TIMER', workMinutes: time, short: shortMinutes, long: longMinutes})
+
+  let time = new Date(Date.now() + longMinutes * 60000);
   startTimer(time);
+}
+
+function pauseTime() {
+  chrome.runtime.sendMessage({ cmd: 'RUNNING' }, response => {
+    //response.time == time left 
+    console.log(response)
+    if (response.time) {
+      if (response.cmd == 'PAUSE') {
+        clearInterval(interval);
+        interval = null;
+        timeConversion(response.time);
+        console.log('front-end paused')
+      } else if (response.cmd == 'RESUME') {
+        startTimer(new Date(response.time));
+        console.log('front-end resumed');
+      }
+    }
+  });
+}
+
+function endSess() {
+  clearInterval(interval);
+  interval = null;
+  countdownEl.innerHTML = `${document.getElementById('workTime').value}:00`;
+  //TODO change this to be just the worktime 
+
+  chrome.runtime.sendMessage({cmd: 'END_TIMER'})
 }
 
 function currEvent() {
@@ -69,3 +105,21 @@ function currEvent() {
     return "lBreak"
   }
 }
+
+function timeConversion(timeMS) {
+  const minutes = Math.floor(timeMS / 60000);
+  let seconds = ((timeMS % 60000) / 1000).toFixed(0);
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  countdownEl.innerHTML = `${minutes}:${seconds}`;
+}
+
+clearInterval(interval);
+interval = null;
+if (numShort < 4) {
+  partition = (partition + 1) % 2;
+} else { //starts the long timer
+  numShort = 0;
+  partition = 2;
+}
+startTime();
